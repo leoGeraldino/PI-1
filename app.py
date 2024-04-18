596c67f6b93f7709d71cbab6c8818d5f5e4f9054
leoGeraldino <leo.nardo_geraldino@hotmail.com>
2024-04-18 10:38:51 -0300

    Adição do módulo session e da secret_key

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask,session
 from views import views
 
 app = Flask(__name__)
+app.secret_key = '1234'
 app.register_blueprint(views, url_prefix='/views')
 
 if __name__ == '__main__':
