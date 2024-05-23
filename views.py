b1e4dc5f9138ed883cfe085222caa30c4f980cb5
leoGeraldino <leo.nardo_geraldino@hotmail.com>
2024-05-23 10:27:58 -0300

    Rotas finalizadas e simplificadas através de algumas funções reutilizáveis

--- views.py
+++ views.py
@@ -11,24 +11,62 @@ configuracao_bd = {
     'raise_on_warnings': True
 }
 
-@views.route('/redirect_registro', methods=['GET', 'POST'])
+def conecta_bd(config):  #função de conectar db abreviado
+    try:
+        conexao = mysql.connector.connect(**configuracao_bd)
+        cursor = conexao.cursor()
+        return conexao, cursor
+    
+    except mysql.connector.Error as e:
+        return "Erro ao conectar-se ao banco de dados:{}".format(e)
+    
+def dados_usuario(): #função de receber os dados do usuário
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    bday = request.form['bday']
+    return username, password, email, bday
+
+def dados_carta():  #função de receber os dados da carta
+    card = request.form['card']
+    preco = request.form['preco']
+    artista = request.form['artista']
+    tipo = request.form['tipo']
+    texto = request.form['texto']
+    lealdade = request.form['lealdade']
+    cmc = request.form['cmc']
+    edicao = request.form['edicao']
+    formato = request.form['formato']
+    raridade = request.form['raridade']
+    cor = request.form['cor']
+    pr = request.form['pr']
+    custos = request.form['custos']
+    return card, preco, artista, tipo, texto, lealdade, cmc, edicao, formato, raridade, cor, pr, custos
+    
+def comita_conexao(conexao, cursor):  #função fazer commit da conexão, fechar cursor e conexão
+    try:
+        conexao.commit()
+    finally:
+        cursor.close()
+        conexao.close()
+
+def fecha_conexao(conexao, cursor):  #função de fechar cursor e conexão
+    try:
+        cursor.close() 
+    finally:    
+        conexao.close()
+
+@views.route('/redirect_registro', methods=['GET', 'POST'])  #rota para redirecionar à página de registro
 def redirect_registro():
     return redirect(url_for('views.registro'))
 
-#rota de registro de usuário
-
-@views.route('/registro.html', methods=['GET', 'POST'])
+@views.route('/registro.html', methods=['GET', 'POST'])  #rota de registro de usuário
 def registro():
     if request.method == 'POST':
-
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        bday = request.form['bday']
+        username, password, email, bday = dados_usuario()
 
         try:
-            conexao = mysql.connector.connect(**configuracao_bd)
-            cursor = conexao.cursor()
+            conexao, cursor = conecta_bd(configuracao_bd)  
             query = "SELECT * FROM usuarios WHERE username = %s"
             cursor.execute(query, (username,))
             usuario_existente = cursor.fetchone()
@@ -39,10 +77,7 @@ def registro():
 
             query = "INSERT INTO usuarios (username, password, email, bday) VALUES (%s, %s, %s, %s)"
             cursor.execute(query, (username, password, email, bday))
-            conexao.commit()
-            cursor.close()
-            conexao.close()
-
+            comita_conexao(conexao, cursor) 
             return redirect(url_for('views.index'))
     
         except mysql.connector.Error as error:
@@ -50,34 +85,23 @@ def registro():
     
     return render_template('registro.html')
 
-# rota de reset de senha
-
-@views.route('/reset.html', methods=['GET', 'POST'])
+@views.route('/reset.html', methods=['GET', 'POST'])  # rota de reset de senha
 def reset():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-        bday = request.form['bday']
+        username, password, email, bday = dados_usuario() 
 
         try:
-            conexao = mysql.connector.connect(**configuracao_bd)
-            cursor = conexao.cursor()
-
+            conexao, cursor = conecta_bd(configuracao_bd)  
             query = "SELECT * FROM usuarios WHERE username = %s AND email = %s AND bday = %s"
             cursor.execute(query, (username, email, bday))
             usuario = cursor.fetchone()
-
             if not usuario:
                 message2 = "Usuário não encontrado. Por favor verifique as informações inseridas."
                 return render_template('reset.html', message2 = message2)
             
             query = "UPDATE usuarios SET password =%s WHERE username = %s"
             cursor.execute(query, (password, username))
-            conexao.commit()
-
-            cursor.close()
-            conexao.close()
+            comita_conexao(conexao, cursor)  
             return redirect(url_for('views.index'))
         
         except mysql.connector.Error as error:
@@ -85,18 +109,14 @@ def reset():
 
     return render_template('reset.html')
 
-# rota de login
-
-@views.route('/index.html', methods=['GET', 'POST']) 
+@views.route('/index.html', methods=['GET', 'POST'])   # rota de login
 def index():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
         try:
-            conexao = mysql.connector.connect(**configuracao_bd)
-            cursor = conexao.cursor()
-
+            conexao, cursor = conecta_bd(configuracao_bd)  
             query = "SELECT idUsuarios FROM usuarios WHERE username = %s AND password = %s"
             cursor.execute(query, (username, password))
             usuario = cursor.fetchone()
@@ -113,9 +133,7 @@ def index():
             return "Erro ao conectar-se ao banco de dados:{}".format(error)
     return render_template('index.html')
 
-# rota de perfil
-
-@views.route('/perfil.html', methods=['GET', 'POST'])
+@views.route('/perfil.html', methods=['GET', 'POST'])  # rota de perfil
 def perfil():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
@@ -125,24 +143,17 @@ def perfil():
     cursor = None
 
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
         
         if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            email = request.form['email']
-            bday = request.form['bday']
+            username, password, email, bday = dados_usuario()
 
             if not username or not password or not email or not bday:
                 return "Por favor, preencha todos os campos"
                 
             query_update = "UPDATE usuarios SET username =%s, password=%s, email=%s, bday=%s WHERE idUsuarios = %s"
             cursor.execute(query_update, (username, password, email, bday, id_usuario))
-            conexao.commit()
-            cursor.close()
-            conexao.close()
-
+            comita_conexao(conexao, cursor)  
             return redirect(url_for('views.perfil', id_usuario = id_usuario)) 
             
         else:
@@ -156,10 +167,7 @@ def perfil():
             query_decks_cadastrados = "SELECT deck_name FROM decks WHERE idUsuario = %s"
             cursor.execute(query_decks_cadastrados, (id_usuario,))
             decks_cadastrados = [row[0] for row in cursor.fetchall()]
-            
-            cursor.close()
-            conexao.close()
-
+            fecha_conexao(conexao, cursor)
             return render_template ('perfil.html', usuario=usuario, decks_cadastrados=decks_cadastrados)
 
     except mysql.connector.Error as error:
@@ -169,9 +177,7 @@ def perfil():
             conexao.close()
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-# rota de adicionar cartas
-
-@views.route('/cartas.html', methods=['GET', 'POST'])
+@views.route('/cartas.html', methods=['GET', 'POST'])  # rota de adicionar cartas
 def cartas():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
@@ -179,30 +185,12 @@ def cartas():
     
     if request.method == 'POST':
         try:
-            conexao = mysql.connector.connect(**configuracao_bd)
-            cursor = conexao.cursor()
-
-            card = request.form['card']
-            preco = request.form['preco']
-            artista = request.form['artista']
-            tipo = request.form['tipo']
-            texto = request.form['texto']
-            lealdade = request.form['lealdade']
-            cmc = request.form['cmc']
-            edicao = request.form['edicao']
-            formato = request.form['formato']
-            raridade = request.form['raridade']
-            cor = request.form['cor']
-            pr = request.form['pr']
-            custos = request.form['custos']
+            conexao, cursor = conecta_bd(configuracao_bd)  
+            card, preco, artista, tipo, texto, lealdade, cmc, edicao, formato, raridade, cor, pr, custos = dados_carta()
 
             query = "INSERT INTO cartas (idUsuario, card, preco, artista, tipo, texto, lealdade, CMC, edicao, formato, raridade, cor, pr, custos) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
             cursor.execute(query, (id_usuario, card, preco, artista, tipo, texto, lealdade, cmc, edicao, formato, raridade, cor, pr, custos))
-            conexao.commit()
-
-            cursor.close()
-            conexao.close()
-
+            comita_conexao(conexao, cursor)
             return redirect(url_for('views.perfil', id_usuario = id_usuario))
 
         except mysql.connector.Error as error:
@@ -210,9 +198,8 @@ def cartas():
         
     else:
         return render_template('cartas.html')
-
-#rota para adicionar decks    
-@views.route('/decks.html', methods = ['GET', 'POST'])
+ 
+@views.route('/decks.html', methods = ['GET', 'POST'])  #rota para adicionar decks   
 def decks():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
@@ -222,8 +209,7 @@ def decks():
     indice_cartas = []
 
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
 
         query = "SELECT idCarta, card FROM cartas WHERE idUsuario = %s"
         cursor.execute(query, (id_usuario,))       
@@ -261,16 +247,14 @@ def decks():
                 message5 = "Por favor, insira o nome do deck e selecione pelo menos uma carta."
                 return render_template('decks.html', lista_cartas=lista_cartas, message5=message5)
 
-        cursor.close()
-        conexao.close()    
+        fecha_conexao(conexao, cursor) 
 
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
     return render_template('decks.html', lista_cartas = lista_cartas)
 
-#rota para deletar decks
-@views.route('/delete_deck', methods=['POST'])
+@views.route('/delete_deck', methods=['POST'])  #rota para deletar decks
 def delete_deck():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
@@ -282,31 +266,24 @@ def delete_deck():
         return "Nome do deck não fornecido."
 
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
 
         query_delete_deck = "DELETE FROM decks WHERE deck_name = %s AND idUsuario =%s"
         cursor.execute(query_delete_deck, (deck_name_delete, id_usuario))
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-
+        comita_conexao(conexao, cursor)  
         return redirect(url_for('views.perfil'))
     
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-#rota para ir para a página de edição de decks
-@views.route('/edit_deck', methods=['GET','POST'])  
+@views.route('/edit_deck', methods=['GET','POST'])  #rota para ir para a página de edição de decks
 def edit_deck():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
         return redirect(url_for('views.index'))
 
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
-        
+        conexao, cursor = conecta_bd(configuracao_bd)  
         deck_name_edit = request.form.get('deck_name_edit')
 
         if request.method == 'POST':
@@ -334,9 +311,7 @@ def edit_deck():
             query_cartas_usuario = "SELECT card FROM cartas WHERE idUsuario = %s"
             cursor.execute(query_cartas_usuario, (id_usuario,))       
             lista_cartas_usuario = cursor.fetchall()
-
-            cursor.close()
-            conexao.close()
+            fecha_conexao(conexao, cursor)
                 
             return render_template('editdeck.html', deck_name_edit=deck_name_edit, 
                                        lista_cartas_usuario=lista_cartas_usuario, 
@@ -346,16 +321,14 @@ def edit_deck():
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-#rota para incluir as alterações no deck e salvá-las
-@views.route('/save_edit_deck', methods = ['GET', 'POST'])
+@views.route('/save_edit_deck', methods = ['GET', 'POST']) #rota para incluir as alterações no deck e salvá-las
 def save_edit_deck():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
         return redirect(url_for('views.index'))
     
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
 
         if request.method == 'POST':
             save_deckname = request.form.get('deck_name')
@@ -379,40 +352,31 @@ def save_edit_deck():
                     query_add_card = "INSERT INTO decks_cartas (idDeck, idCarta) VALUES (%s, %s)"
                     cursor.execute(query_add_card, (id_deck, id_carta_retornada[0]))
 
-            conexao.commit()
-            cursor.close()
-            conexao.close()
-
+            comita_conexao(conexao, cursor)  
             return redirect(url_for('views.perfil'))
 
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-#rota para permitir a alteração das cartas
-@views.route('/editar_cartas', methods = ['GET', 'POST'])
+@views.route('/editar_cartas', methods = ['GET', 'POST'])  #rota para permitir a alteração das cartas
 def editar_cartas():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
         return redirect(url_for('views.index'))
     
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd) 
         
         query_cartas_usuario = "SELECT card FROM cartas WHERE idUsuario = %s"
         cursor.execute(query_cartas_usuario, (id_usuario,))       
         lista_cartas_usuario = cursor.fetchall()
-
-        cursor.close()
-        conexao.close()
-        
+        fecha_conexao(conexao, cursor)
         return render_template('editarcartas.html', lista_cartas_usuario=lista_cartas_usuario)
     
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
-
-#rota para exclusão de cartas    
-@views.route('delete_card', methods=['POST'])
+   
+@views.route('delete_card', methods=['POST'])  #rota para exclusão de cartas 
 def delete_card():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
@@ -424,85 +388,57 @@ def delete_card():
         return "Nome do card não fornecido."
 
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
-
+        conexao, cursor = conecta_bd(configuracao_bd)  
         query_delete_card = "DELETE FROM cartas WHERE card = %s AND idUsuario =%s"
         cursor.execute(query_delete_card, (card_name_delete, id_usuario))
-        conexao.commit()
-        cursor.close()
-        conexao.close()
-
+        comita_conexao(conexao, cursor)  
         return redirect(url_for('views.editar_cartas'))
     
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-#rota para alterar cartas
-@views.route('edit_card', methods=['GET', 'POST'])
+@views.route('edit_card', methods=['GET', 'POST'])  #rota para alterar cartas
 def edit_card():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
         return redirect(url_for('views.index'))
     
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
 
         if request.method == 'POST':
             card_name_edit = request.form.get('card_name_edit')
             query_editar_carta = "SELECT * FROM cartas WHERE card =%s AND idUsuario =%s"
             cursor.execute(query_editar_carta, (card_name_edit, id_usuario))
             card_info = cursor.fetchone()
-            cursor.close()
-            conexao.close()
-
+            fecha_conexao(conexao, cursor)
             return render_template ('editcard.html', card_info = card_info)
 
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
 
-#rota para salvar alterações nas cartas
-@views.route('salvar_edit_card', methods=['GET', 'POST'])
+@views.route('salvar_edit_card', methods=['GET', 'POST'])  #rota para salvar alterações nas cartas
 def salvar_edit_card():
     id_usuario = session.get('id_usuario')
     if id_usuario is None:
         return redirect(url_for('views.index'))
     
     try:
-        conexao = mysql.connector.connect(**configuracao_bd)
-        cursor = conexao.cursor()
+        conexao, cursor = conecta_bd(configuracao_bd)  
 
         if request.method == 'POST':
             id_carta = request.form['id_carta']
-            card = request.form['card']
-            preco = request.form['preco']
-            artista = request.form['artista']
-            tipo = request.form['tipo']
-            texto = request.form['texto']
-            lealdade = request.form['lealdade']
-            cmc = request.form['cmc']
-            edicao = request.form['edicao']
-            formato = request.form['formato']
-            raridade = request.form['raridade']
-            cor = request.form['cor']
-            pr = request.form['pr']
-            custos = request.form['custos']
-
-            query_atualiza_carta = "UPDATE cartas SET card = %s, preco = %s, artista = %s, tipo = %s, texto = %s, lealdade = %s, CMC = %s, edicao = %s, formato = %s, raridade = %s, cor = %s, pr = %s, custos = %s WHERE idUsuario = %s AND idCarta = %s"
+            card, preco, artista, tipo, texto, lealdade, cmc, edicao, formato, raridade, cor, pr, custos = dados_carta()
             
+            query_atualiza_carta = "UPDATE cartas SET card = %s, preco = %s, artista = %s, tipo = %s, texto = %s, lealdade = %s, CMC = %s, edicao = %s, formato = %s, raridade = %s, cor = %s, pr = %s, custos = %s WHERE idUsuario = %s AND idCarta = %s"
             cursor.execute(query_atualiza_carta, (card, preco, artista, tipo, texto, lealdade, cmc, edicao, formato, raridade, cor, pr, custos, id_usuario, id_carta))
-            conexao.commit()
-            cursor.close()
-            conexao.close()
-
+            comita_conexao(conexao, cursor)  
             return redirect(url_for('views.perfil'))
 
     except mysql.connector.Error as error:
         return "Erro ao conectar-se ao banco de dados:{}".format(error)
     
-@views.route('/logout')
+@views.route('/logout')  #rota para logout do sistema
 def logout():
-    session.pop('id_usuario', None)  # Remove 'id_usuario' da sessão
-    return redirect(url_for('views.index'))  # Redireciona para a página inicial
-            
+    session.pop('id_usuario', None)  
+    return redirect(url_for('views.index'))  
